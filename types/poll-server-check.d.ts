c338ce6dadc1f94f7386f370aba45de6612257d2
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-09-21 19:35:07 +0530

    Update poll-server-check.d.ts

--- types/poll-server-check.d.ts
+++ types/poll-server-check.d.ts
@@ -1,4 +1,4 @@
 declare module "poll-server-check" {
-  export function clientPoll(BackendUrl: string): void;
+  export function clientCheck(BackendUrl: string): void;
   export function serverCheck(app: any): void;
 }
